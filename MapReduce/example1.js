9b7fe5666f83da4b94b154ddc55ff96b9c7177fb
Mauro Rainis <mauro.rainis@gmail.com>
2022-04-24 16:53:19 +0200

    Aggiunto esempio map reduce javascript

---
+++ MapReduce/example1.js
@@ -0,0 +1,26 @@
+const persone = require('./Samples/Persone_eta_altezza.json')
+
+// Map - Estraggo le informazioni necessarie
+const data = persone.map(function(item) {
+  return { "eta": item.Age, "altezza": item.Height }
+});
+
+let selected = 0;
+
+// Reduce - Calcolo i risultati
+const media = data.reduce(function (result, item, index, values) {
+  if (item.eta > 35) 
+  {
+    selected++;
+    result += item.altezza;
+  }
+  
+  if (index == values.length -1) 
+  {
+    result = result / selected;
+  }
+    
+  return result;
+}, 0);
+
+console.log('altezza media degli over 35: ' + media);
